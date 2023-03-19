acb8f683fae716bdf76b1921ed7ff78d17ca8f6b
Omotunde Shitta <103114829+oashitta@users.noreply.github.com>
2023-03-19 15:16:27 -0400

    Added more consitional examples and logical operators

--- conditionals.js
+++ conditionals.js
@@ -1,6 +1,7 @@
+// weather
 const raining = false;
 const cold = false;
-const temperature = 12;
+const temperature = 42;
 
 if (temperature < 0) {
   conaole.log("make sure you pick out a scarf");
@@ -21,3 +22,22 @@ if (cold) {
 }
 
 console.log("Now you're ready to go outside!");
+
+// using logical operators
+if (temperature < -40 || temperature > 40) {
+  console.log("Maybe it is not a good idea to go out.");
+}
+
+if (!raining) {
+  console.log("Leave your umbrella at home.");
+}
+
+// voting rights
+
+const isCitizen = true;
+const age = 30;
+
+// using logical operators
+if (isCitizen && age > 18) {
+  console.log("you are eligible to vote.");
+}
