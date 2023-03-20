04803cab70e8c311b52b57044e6f1abe4cde59d2
Omotunde Shitta <103114829+oashitta@users.noreply.github.com>
2023-03-20 14:01:49 -0400

    functions lesson

--- chorus-while.js
+++ chorus-while.js
@@ -2,7 +2,7 @@ const chorus = "Let's dance!";
 let repeat = 0;
 while (repeat < 10) {
   if (repeat === 5) {
-    console.log("*Change Key*");
+    console.log("Change Key");
   }
   console.log(chorus);
   repeat++;
---
+++ hello.js
@@ -0,0 +1,11 @@
+const sayHello = function () {
+  console.log("Hello, world");
+};
+
+sayHello();
+
+const sayHi = function (name) {
+  console.log("Hello, " + name);
+};
+
+sayHi("jim");
